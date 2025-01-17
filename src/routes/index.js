2bb5ca67d07650adf5db8e99bb340afa2afb8383
LuiferEduardoo <luifer01ortegaperez@gmail.com>
2025-01-16 20:56:18 -0500

    feat: add callback

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 
 import { Login } from '../services/login.js';
+import callback from '../services/callback.js';
 
 const routerApi = (app) => {
   app.get("/api/v1/login", Login);
+  app.get("/api/v1/callback", callback);
 }
 
 
---
+++ src/services/callback.js
@@ -0,0 +1,35 @@
+import querystring from "node:querystring";
+import { encode } from "../utils/encode.js";
+
+const SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token";
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default async function callback(req, res) {
+  const clientAuth = encode(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`)
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${clientAuth}`,
+    },
+    body: querystring.stringify({
+      code: req.query.code,
+      redirect_uri: process.env.REDIRECT_URI,
+      grant_type: "authorization_code",
+    })
+  };
+
+  try {
+    const response = await fetch(SPOTIFY_TOKEN_URL, options);
+    const data = await response.json();
+
+    res.setHeader("Set-Cookie", `access_token=${data.access_token}; Path=/; HttpOnly`);
+
+    res.writeHead(302, { Location: process.env.FRONTEND_URI});
+    res.end();
+  } catch (error) {
+    console.error(error);
+  }
+}
